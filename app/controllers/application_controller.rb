78341ecd41627b2e97b9ef3dd0cc97766c183da1
Edwin Sandoval <edwinsandoval.428@gmail.com>
2017-02-01 17:15:48 -0500

    headers delete

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,10 +5,14 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :setbrands
   before_action :setadvertisements
+  before_filter :allow_iframe_requests
+
   
 
   protected
-
+  def allow_iframe_requests
+    response.headers.delete('X-Frame-Options')
+  end
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name,:photo,:city,:celular,:facebook])
     devise_parameter_sanitizer.permit(:account_update, keys: [:name,:photo,:city,:celular,:facebook])
